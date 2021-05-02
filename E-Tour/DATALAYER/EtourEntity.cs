18fd15cb82019712df5c1fa10c4425a657270d4f
Thắng Nguyễn Võ Đức <19522213@ms.uit.edu.vn>
2021-05-02 13:24:24 +0700

    Entityframework - Thêm

---
+++ E-Tour/DATALAYER/EtourEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Tour.DATALAYER
+{
+    public class EtourEntity
+    {
+        public static EtourEnties Entity = new EtourEnties();
+    }
+}
